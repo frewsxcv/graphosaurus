2c9dfdf3a8f21f2fa8c69ed39634122a11252868
Corey Farwell <coreyf@rwell.org>
2013-12-02 13:30:32 -0800

    Don't display almond license comment in minified

--- build/config.js
+++ build/config.js
@@ -3,6 +3,7 @@
     name: 'build/almond/almond',
     include: ['src/graphosaurus'],
     out: '../dist/graphosaurus.min.js',
+    preserveLicenseComments: false,
     wrap: {
         startFile: 'start.frag.js',
         endFile: 'end.frag.js'
