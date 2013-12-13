b76b774380871b367605d962fa140bfa2f0ac773
Corey Farwell <coreyf@rwell.org>
2013-12-13 12:09:16 -0800

    Allow node id when creating edges

--- src/edge.js
+++ src/edge.js
@@ -15,8 +15,19 @@ define(function () {
     };
 
     Edge.prototype.addTo = function (graph) {
+        this._resolveIds(graph);
         graph.addEdge(this);
         return this;
+    };
+
+    Edge.prototype._resolveIds = function (graph) {
+        if (typeof this._nodes[0] === "string") {
+            this._nodes[0] = graph.getNode(this._nodes[0]);
+        }
+
+        if (typeof this._nodes[1] === "string") {
+            this._nodes[1] = graph.getNode(this._nodes[1]);
+        }
     };
 
     return Edge;
