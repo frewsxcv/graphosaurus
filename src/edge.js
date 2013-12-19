9611e4be52e6ee647ce5810331ba5b1fd794b35b
Corey Farwell <coreyf@rwell.org>
2013-12-18 23:04:51 -0800

    Node IDs in Edges should be resolved in Graph

--- src/edge.js
+++ src/edge.js
@@ -36,29 +36,8 @@ define(function () {
     };
 
     Edge.prototype.addTo = function (graph) {
-        this._resolveIds(graph);
         graph.addEdge(this);
         return this;
-    };
-
-    /**
-     * Replace string IDs representing nodes with node references
-     * @private
-     * @method
-     *
-     * @param {G.Graph} graph - Graph to retrieve node references from node IDs
-     * @returns {undefined}
-     */
-    Edge.prototype._resolveIds = function (graph) {
-        var type = typeof this._nodes[0];
-        if (type === "string" || type === "number") {
-            this._nodes[0] = graph.getNode(this._nodes[0]);
-        }
-
-        type = typeof this._nodes[1];
-        if (type === "string" || type === "number") {
-            this._nodes[1] = graph.getNode(this._nodes[1]);
-        }
     };
 
     return Edge;
