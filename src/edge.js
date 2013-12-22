e07b3c30387f43990f6c4bb4c4617ffd5b3f6f4f
Corey Farwell <coreyf@rwell.org>
2013-12-21 18:47:44 -0800

    Add documentation for edge'

--- src/edge.js
+++ src/edge.js
@@ -1,11 +1,26 @@
 define(function () {
     "use strict";
 
+    /**
+     * Constructs a new Edge
+     * @constructor
+     * @alias Edge
+     *
+     * @param {Array} nodes - Nodes
+     * @param {Object} opts - Options
+     */
     var Edge = function (nodes, opts) {
         this.setNodes(nodes);
         this._initOpts(opts);
     };
 
+    /**
+     * Initialize Edge options
+     * @private
+     *
+     * @param {Object} options - Options passed in from the constructor
+     * @returns {Edge} The Edge the method was called on
+     */
     Edge.prototype._initOpts = function (options) {
         options = options || {};
 
@@ -15,15 +30,43 @@ define(function () {
         return this;
     };
 
+    /**
+     * Set the nodes of the Edge
+     *
+     * @param {Array} nodes - Array of two [Nodes]{@link module:Node}
+     * @returns {Edge} The Edge the method was called on
+     */
     Edge.prototype.setNodes = function (nodes) {
         this._nodes = nodes;
         return this;
     };
 
+    /**
+     * Get the nodes of the Edge
+     *
+     * @returns {Array} Array of two G.Nodes that represent the ends of the
+     *     Edge
+     */
     Edge.prototype.getNodes = function () {
+        // TODO: should this do this._nodes.clone(); ?
         return this._nodes;
     };
 
+    /**
+     * Set the color of the Edge
+     *
+     * @param {Number|String} color - Hexadecimal or CSS-style string representation of a color
+     * @returns {Edge} The Edge the method was called on
+     *
+     * @example
+     * myEdge.setColor(0x00ffbb);
+     *
+     * @example
+     * myEdge.setColor("rgb(250,0,0)");
+     *
+     * @example
+     * myEdge.setColor("green");
+     */
     Edge.prototype.setColor = function (color) {
         if (color) {
             this._color.set(color);
@@ -31,10 +74,26 @@ define(function () {
         return this;
     };
 
+    /**
+     * Gets the color of the Edge
+     *
+     * @returns {String} String hexadecimal representation of the Edge's color
+     *
+     * @example
+     * myEdge.setColor("green");
+     * myEdge.getColor();  // returns "008000"
+     */
     Edge.prototype.getColor = function () {
         return this._color.getHexString();
     };
 
+    /**
+     * Add the Edge to a Graph
+     * ADD NOTE ABOUT EDGE IDS BEING RESOLVED
+     *
+     * @param {Graph} graph - Graph the Edge will be added to
+     * @returns {Edge} The Edge the method was called on
+     */
     Edge.prototype.addTo = function (graph) {
         graph.addEdge(this);
         return this;
