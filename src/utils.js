be783289ba6a22daf0c5b0245964f2587b96eb39
Corey Farwell <coreyf@rwell.org>
2014-05-15 21:43:57 -0700

    Convert to doc comment

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,9 @@
 define(function () {
     "use strict";
 
-    // Allows constructors to be called without using `new`
+    /**
+     * Allows constructors to be called without using `new`
+     */
     var noNew = function (Constructor) {
         return function () {
             var instance = Object.create(Constructor.prototype);
